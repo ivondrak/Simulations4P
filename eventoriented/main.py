288869ad733defafe4c65adb0763ab199ff6a268
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-04-28 19:12:23 +0200

    Uniform distribution added

--- eventoriented/main.py
+++ eventoriented/main.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('.')
 
 from simulation_app import SimulationApp, GeneratorFactory
-from utilities.generator import ExponentialGenerator
+from utilities.generator import ExponentialGenerator, UniformGenerator, NormalGenerator
 
 class ExponentialFactory(GeneratorFactory):
     
@@ -10,10 +10,18 @@ class ExponentialFactory(GeneratorFactory):
         return ExponentialGenerator(1.0, self.number_of_samples, self.seed)
     
     def createServices(self):
-        return ExponentialGenerator(0.5, self.number_of_samples, self.seed)
+        return ExponentialGenerator(0.7, self.number_of_samples, self.seed)
+    
+class UniformFactory(GeneratorFactory):
+    
+    def createArrivals(self):
+        return UniformGenerator(0, 20, self.number_of_samples, self.seed)
+    
+    def createServices(self):
+        return UniformGenerator(2, 5, self.number_of_samples, self.seed)
 
 if __name__ == "__main__":
     title = 'Single queue single service simulation'
     number_of_samples = 1000
-    app = SimulationApp(title, ExponentialFactory(number_of_samples, 1959))
+    app = SimulationApp(title, UniformFactory(number_of_samples, 1959))
     app.run()
