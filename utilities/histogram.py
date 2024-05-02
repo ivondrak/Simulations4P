85dd6cde88cf3d757a98952d02ad5a03e4cf874a
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-05-02 09:03:36 +0200

    Jupyter notebook for event-orientes simulation added

--- utilities/histogram.py
+++ utilities/histogram.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 
 class Histogram:
-    def __init__(self, data):
+    def __init__(self, data, title='Histogram', x_label='Values', y_label='Frequency'):
         self.data = data
+        self.title = title
+        self.x_label = x_label
+        self.y_label = y_label
 
     def plot(self):
         plt.hist(self.data, bins='auto')
-        plt.xlabel('Values')
-        plt.ylabel('Frequency')
-        plt.title('Histogram')
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
+        plt.title(self.title)
         plt.grid(True)
         plt.show()
